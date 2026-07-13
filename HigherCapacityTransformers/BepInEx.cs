b59534b8620bee1999d2cf57a2772b4756f5f930
Aral <araltan@hotmail.com>
2026-07-13 15:24:07 +0300

    1.0 Release Version
    - Moved from unity mod to BepInEx
    - Implemented target feature at a basic level

---
+++ HigherCapacityTransformers/BepInEx.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HigherCapacityTransformers
+{
+    #region BepInEx
+    [BepInEx.BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class BepInExHigherCapacityTransformers : BepInEx.BaseUnityPlugin
+    {
+        public const string pluginGuid = "aspctt.highercapacitytransformers";
+        public const string pluginName = "HigherCapacityTransformers";
+        public const string pluginVersion = "1.0";
+        public static void Log(string line)
+        {
+            Debug.Log("[" + pluginName + "]: " + line);
+        }
+        void Awake()
+        {
+            try
+            {
+                var harmony = new Harmony(pluginGuid);
+                harmony.PatchAll();
+                Log("Patch succeeded");
+
+            }
+            catch (Exception e)
+            {
+
+                Log("Patch Failed");
+                Log(e.ToString());
+            }
+        }
+    }
+    #endregion
+}
---
+++ HigherCapacityTransformers/HigherCapacityTransformersMod.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Scripts.Objects;
+using Assets.Scripts.Objects.Electrical;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HigherCapacityTransformers
+{
+    [HarmonyPatch(typeof(Prefab), "LoadCorePrefabs")]
+    public class AddStructureIntoKit
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            Transformer MediumTransformerReversed = Prefab.Find<Transformer>("StructureTransformerMedium(Reversed)");
+            Transformer MediumTransformer = Prefab.Find<Transformer>("StructureTransformerMedium");
+            Transformer LargeTransformer = Prefab.Find<Transformer>("StructureTransformer");
+
+            if (MediumTransformer != null)
+            {
+                MediumTransformer.OutputMaximum = 100000f;
+            }
+
+            if (MediumTransformerReversed != null)
+            {
+                MediumTransformerReversed.OutputMaximum = 100000f;
+            }
+
+            if (LargeTransformer != null)
+            {
+                LargeTransformer.OutputMaximum = 500000f;
+                LargeTransformer.StepNormal = 5000f;
+                LargeTransformer.StepSmall = 500f;
+            }
+        }
+    }
+}
